93e8d2e91db6074609dc38a91d796046b073b392
Paige Lindahl <paige@peakfifteen.co.uk>
2021-11-30 14:07:51 +0000

    adds animation and styling to nav

--- front-end/src/components/Navigate.jsx
+++ front-end/src/components/Navigate.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components';
+import '../styles/Nav.scss'
 
 export default function Navigate(props) {
 
 
+
+
   return (
     <>
-      <Nav className="justify-content-end" activeKey="/home">
+      <Nav className="justify-content-end navigation" activeKey="/home">
         <Nav.Item>
-          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link className="nav-link" href="/home">Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">About</Nav.Link>
+          <Nav.Link className="nav-link" eventKey="link-1">About</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">Projects</Nav.Link>
+          <Nav.Link className="nav-link" eventKey="link-2">Projects</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3">
+          <Nav.Link className="nav-link" eventKey="link-3">
             Contact
           </Nav.Link>
         </Nav.Item>
