00685528400292a712cf8d1a646fa0a1a38774da
paigelindahl <paigemarlindahl@gmail.com>
2021-12-13 12:08:06 +0000

    removes commented out code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import Navigate from './components/Navigate';
 import HomePage from './components/HomePage';
 import About from './components/About';
@@ -7,18 +7,11 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 
-// const useMountEffect = fun => useEffect(fun, []);
 
 function App() {
-  // const myRef = useRef(null);
-
-  // const executeScroll = () => {
-  //   myRef.current.scrollIntoView();
-  //   console.log('my ref current', myRef.current)
-  // }
 
 
-  // useMountEffect(executeScroll);
+
   return (
     <div className="App">
       <Navigate></Navigate>
