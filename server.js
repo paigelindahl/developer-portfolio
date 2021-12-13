9fb5d1c1e87245407147eba7fa3e7f31aa3c953d
paigelindahl <paigemarlindahl@gmail.com>
2021-12-13 14:04:05 +0000

    heroku edits

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
+const path = require('path');
 
 
 const app = express();
