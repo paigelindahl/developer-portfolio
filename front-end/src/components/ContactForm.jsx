4f8d2aa75142896c39d0679b2ec4b6fa358d9b34
paigelindahl <paigemarlindahl@gmail.com>
2021-12-14 11:45:00 +0000

    changes route for email message

--- front-end/src/components/ContactForm.jsx
+++ front-end/src/components/ContactForm.jsx
@@ -17,7 +17,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch(`http://localhost:${port}/contact`, {
+    let response = await fetch("/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
